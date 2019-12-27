8a650afc18b47d139ef076fcf1f7d07e45434410
yolondono <pruebasyonaldev@gmail.com>
2019-12-27 17:54:13 -0500

    Asignacion de los cursos al estudiante

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -10,6 +10,14 @@ public class Principal {
 
         estu1 = new Estudiante("yonal", "Londoño", 1);
 
+        estu1.asignarCurso(estu1.getCurso1());
+        estu1.asignarCurso(estu1.getCurso2());
+        estu1.asignarCurso(estu1.getCurso3());
+        estu1.asignarCurso(estu1.getCurso4());
+        estu1.asignarCurso(estu1.getCurso5());
+        
+        estu1.asignarCurso(estu1.getCurso3());
+
     }
 
 }
--- src/modelo/Estudiante.java
+++ src/modelo/Estudiante.java
@@ -8,7 +8,7 @@ public class Estudiante {
     private String nombre;
     private String apellido;
 
-    String[] estu1 = new String[3];
+    String[][] estu1 = new String[6][4];
 
     Curso cursos;
 
@@ -29,13 +29,16 @@ public class Estudiante {
         this.apellido = apellido;
         this.codigo = codigo;
 
-        estu1[0] = nombre;
-        estu1[1] = apellido;
-        estu1[2] = Integer.toString(codigo);
+        estu1[0][0] = Integer.toString(codigo);
+        estu1[0][1] = nombre;
+        estu1[0][2] = apellido;
 
-        for (String e :
-                estu1) {
-            System.out.println(e);
+        for (int i = 0; i <= 0; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (estu1[i][j] != null) {
+                    System.out.println(estu1[i][j]);
+                }
+            }
         }
 
     }
@@ -52,7 +55,59 @@ public class Estudiante {
         return cursos;
     }
 
+    int cont = 0;
     public boolean asignarCurso(Curso curso) {
+
+
+        for (int i = 1; i < 6; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (estu1[i][j] == null){
+                    break;
+                }
+                if (estu1[i][j].equals(curso.getNombreCurso())) {
+                    System.out.println("El curso " + estu1[i][j] +" ya esta asignado!! ");
+                    return false;
+                }
+            }
+        }
+
+
+        if (curso.getNombreCurso().equals(getCurso1().getNombreCurso())) {
+            estu1[1][0] = Integer.toString(getCurso1().getCodigoCurso());
+            estu1[1][1] = getCurso1().getNombreCurso();
+            estu1[1][2] = Integer.toString(getCurso1().getCreditos());
+            estu1[1][3] = getCurso1().getDescripcion();
+            System.out.println("El curso " + getCurso1().getNombreCurso() + " fue asignado");
+        }
+        if (curso.getNombreCurso().equals(getCurso2().getNombreCurso())) {
+            estu1[2][0] = Integer.toString(getCurso2().getCodigoCurso());
+            estu1[2][1] = getCurso2().getNombreCurso();
+            estu1[2][2] = Integer.toString(getCurso2().getCreditos());
+            estu1[2][3] = getCurso2().getDescripcion();
+            System.out.println("El curso " + getCurso2().getNombreCurso() + " fue asignado");
+        }
+        if (curso.getNombreCurso().equals(getCurso3().getNombreCurso())) {
+            estu1[3][0] = Integer.toString(getCurso3().getCodigoCurso());
+            estu1[3][1] = getCurso3().getNombreCurso();
+            estu1[3][2] = Integer.toString(getCurso3().getCreditos());
+            estu1[3][3] = getCurso3().getDescripcion();
+            System.out.println("El curso " + getCurso3().getNombreCurso() + " fue asignado");
+        }
+        if (curso.getNombreCurso().equals(getCurso4().getNombreCurso())) {
+            estu1[4][0] = Integer.toString(getCurso4().getCodigoCurso());
+            estu1[4][1] = getCurso4().getNombreCurso();
+            estu1[4][2] = Integer.toString(getCurso4().getCreditos());
+            estu1[4][3] = getCurso4().getDescripcion();
+            System.out.println("El curso " + getCurso4().getNombreCurso() + " fue asignado");
+        }
+        if (curso.getNombreCurso().equals(getCurso5().getNombreCurso())) {
+            estu1[5][0] = Integer.toString(getCurso5().getCodigoCurso());
+            estu1[5][1] = getCurso5().getNombreCurso();
+            estu1[5][2] = Integer.toString(getCurso5().getCreditos());
+            estu1[5][3] = getCurso5().getDescripcion();
+            System.out.println("El curso " + getCurso5().getNombreCurso() + " fue asignado");
+        }
+
 
         return true;
     }
